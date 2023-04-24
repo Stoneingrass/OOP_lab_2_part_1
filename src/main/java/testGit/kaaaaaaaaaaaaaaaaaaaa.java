f281df7af725ad22877e4bb286b86b6f9c39a055
Illya <svirillya@gmail.com>
2023-04-24 19:28:01 +0300

    Test git commit 2

---
+++ src/main/java/testGit/kaaaaaaaaaaaaaaaaaaaa.java
@@ -0,0 +1,30 @@
+package testGit;
+
+public class kaaaaaaaaaaaaaaaaaaaa {
+    int qweqwe9;
+     String lksladjl;
+    public kaaaaaaaaaaaaaaaaaaaa() {
+    }
+
+    public kaaaaaaaaaaaaaaaaaaaa(int qweqwe9, String lksladjl) {
+        this.qweqwe9 = qweqwe9;
+        this.lksladjl = lksladjl;
+    }
+
+    public int getQweqwe9() {
+        return qweqwe9;
+    }
+
+    public void setQweqwe9(int qweqwe9) {
+        this.qweqwe9 = qweqwe9;
+    }
+
+    public String getLksladjl() {
+        return lksladjl;
+    }
+
+    public void setLksladjl(String lksladjl) {
+        this.lksladjl = lksladjl;
+    }
+}
+
